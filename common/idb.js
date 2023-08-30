c539efb8754f1ded12f14344dd1ad8740bd2fea8
Rafa Ruiz <raruizmu@gmail.com>
2023-08-30 09:48:49 +0200

    Update idb.js (#13)
    * Update idb.js
    
    When closing the database, make it null in order to create a new one when logging in again.
    
    * Update package.json
    
    Bump version
    
    * Bump version

--- common/idb.js
+++ common/idb.js
@@ -1,40 +1,56 @@
-import { deleteDB, openDB } from 'idb';
+import {deleteDB, openDB} from 'idb';
+
 const databaseObjectKey = "token";
 const databaseName = "Doordeck";
 
-const dbPromise = openDB(databaseName, 1, {
-    upgrade(db) {
-        db.createObjectStore(databaseObjectKey);
-    },
-});
+var _dbPromise = null;
+
+function _recreateDatabase() {
+    return openDB(databaseName, 1, {
+        upgrade(db) {
+            db.createObjectStore(databaseObjectKey);
+        },
+    });
+}
+
+async function dbPromise() {
+    if (_dbPromise == null) {
+        _dbPromise = _recreateDatabase();
+    }
+
+    return await _dbPromise
+}
 
 async function get(key) {
-    return (await dbPromise).get(databaseObjectKey, key);
+    return (await dbPromise()).get(databaseObjectKey, key);
 }
 
 async function set(key, val) {
-    (await dbPromise).delete(databaseObjectKey, key);
-    return (await dbPromise).put(databaseObjectKey, val, key);
+    (await dbPromise()).delete(databaseObjectKey, key);
+    return (await dbPromise()).put(databaseObjectKey, val, key);
 }
 
 async function keys() {
-    return (await dbPromise).getAllKeys(databaseObjectKey);
+    return (await dbPromise()).getAllKeys(databaseObjectKey);
 }
+
 async function getAll() {
-    return (await dbPromise).getAll(databaseObjectKey);
+    return (await dbPromise()).getAll(databaseObjectKey);
 
 }
+
 export async function del(key) {
-    return (await dbPromise).delete(databaseObjectKey, key);
+    return (await dbPromise()).delete(databaseObjectKey, key);
 }
 
 export async function deleteDatabase() {
     // Get the DB
-    const db = await dbPromise;
+    const db = await dbPromise();
     // Close all the connections first
     db.close();
     // Then delete the DB.
-    return await deleteDB(databaseName);
+    await deleteDB(databaseName);
+    _dbPromise = null;
 }
 
 export default { get, set, keys, getAll, del, deleteDatabase }
