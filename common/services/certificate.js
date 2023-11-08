7424c11cee7febc6ae3afa7f8cad537b7a2edc18
Gunhan Sancar <gunhan.sancar@superawesome.com>
2023-11-08 09:16:44 +0000

    fix(): Fixed endpoints where the baseurl was static

--- common/services/certificate.js
+++ common/services/certificate.js
@@ -1,4 +1,4 @@
-import {AUTH_TOKEN, BASE_URL, CERT} from '../constants'
+import {AUTH_TOKEN, CERT} from '../constants'
 
 import IDB from '../idb'
 import axios from 'axios'
@@ -13,124 +13,123 @@ import {fromBER} from "asn1js";
  * if this cert is expiring in less than a week. If so, we'll
  * delete it, and we'll return @null to force a new certificate
  */
+const _certAccordingToValidity = async function (certObject) {
+    let base64DerCertificate = certObject["certificateChain"][0];
+    const base64DerCertificateArrayBuffer = Uint8Array.from(atob(base64DerCertificate), c => c.charCodeAt(0)).buffer;
 
-const _certAccordingToValidity = async function(certObject) {
-  let base64DerCertificate = certObject["certificateChain"][0];
-  const base64DerCertificateArrayBuffer = Uint8Array.from(atob(base64DerCertificate), c => c.charCodeAt(0)).buffer;
+    try {
+        const asn1 = fromBER(base64DerCertificateArrayBuffer);
+        const cert = new Certificate({schema: asn1.result});
 
-  try {
-    const asn1 = fromBER(base64DerCertificateArrayBuffer);
-    const cert = new Certificate({ schema: asn1.result });
+        const endDate = cert.notAfter.value;
+        const currentDate = new Date();
+        const weekFromNow = new Date(currentDate);
+        weekFromNow.setDate(currentDate.getDate() + 7);
 
-    const endDate = cert.notAfter.value;
-    const currentDate = new Date();
-    const weekFromNow = new Date(currentDate);
-    weekFromNow.setDate(currentDate.getDate() + 7);
-
-    if (endDate < weekFromNow) {
-      // Delete cert, return null to force getting a new one.
-      await deleteCert();
-      return null;
-    } else {
-      return certObject;
+        if (endDate < weekFromNow) {
+            // Delete cert, return null to force getting a new one.
+            await deleteCert();
+            return null;
+        } else {
+            return certObject;
+        }
+    } catch (error) {
+        // We don't care, there wasn't probably a proper cert here
+        return null;
     }
-  } catch (error) {
-    // We don't care, there wasn't probably a proper cert here
-    return null;
-  }
 }
 
 const retrieveSavedCert = async function () {
-  const email = localStorage.email;
-  if(!email) return null
-  const cert = await IDB.get(email)
-  if(cert) {
-    localStorage[CERT] = JSON.stringify(cert)
-    return await _certAccordingToValidity(cert)
-  }
-  return null
+    const email = localStorage.email;
+    if (!email) return null
+    const cert = await IDB.get(email)
+    if (cert) {
+        localStorage[CERT] = JSON.stringify(cert)
+        return await _certAccordingToValidity(cert)
+    }
+    return null
 }
 const storeCert = function (cert) {
-  localStorage[CERT] = JSON.stringify(cert)
-  // update cert in indexedDB
-  const email = localStorage.email;
-  IDB.set(email, cert)
+    localStorage[CERT] = JSON.stringify(cert)
+    // update cert in indexedDB
+    const email = localStorage.email;
+    IDB.set(email, cert)
 }
 
-const deleteCert = async function() {
-  localStorage.removeItem(CERT);
-  await IDB.deleteDatabase();
+const deleteCert = async function () {
+    localStorage.removeItem(CERT);
+    await IDB.deleteDatabase();
 }
 
 const registerCertificate = function (pubKey) {
-  var ephKey = doordeck.libSodium.to_base64(pubKey, doordeck.libSodium.base64_variants.ORIGINAL)
-  return axios.post(BASE_URL + '/auth/certificate', {
-    'ephemeralKey': ephKey
-  }, {
-    headers: {
-      'Authorization': 'Bearer ' + localStorage[AUTH_TOKEN]
-    }
-  })
+    const ephKey = doordeck.libSodium.to_base64(pubKey, doordeck.libSodium.base64_variants.ORIGINAL);
+    return axios.post(doordeck.getBaseUrl() + '/auth/certificate', {
+        'ephemeralKey': ephKey
+    }, {
+        headers: {
+            'Authorization': 'Bearer ' + localStorage[AUTH_TOKEN]
+        }
+    })
 }
-const registerCertificate2FA = function(pubKey) {
-  var ephKey = doordeck.libSodium.to_base64(pubKey, doordeck.libSodium.base64_variants.ORIGINAL)
-  return axios.post(BASE_URL + '/auth/certificate/verify', {
-    'ephemeralKey': ephKey
-  }, {
-    headers: {
-      'Authorization': 'Bearer ' + localStorage[AUTH_TOKEN]
-    }
-  })
+const registerCertificate2FA = function (pubKey) {
+    const ephKey = doordeck.libSodium.to_base64(pubKey, doordeck.libSodium.base64_variants.ORIGINAL);
+    return axios.post(doordeck.getBaseUrl() + '/auth/certificate/verify', {
+        'ephemeralKey': ephKey
+    }, {
+        headers: {
+            'Authorization': 'Bearer ' + localStorage[AUTH_TOKEN]
+        }
+    })
 }
-const verify2FA = function(code, priKey) {
-  var signature = doordeck.libSodium.crypto_sign_detached(code, priKey)
-  return axios.post(BASE_URL + '/auth/certificate/check', {
-    'verificationSignature': doordeck.libSodium.to_base64(signature, doordeck.libSodium.base64_variants.ORIGINAL)
-  }, {
-    headers: {
-      'Authorization': 'Bearer ' + localStorage[AUTH_TOKEN]
-    }
-  })
+const verify2FA = function (code, priKey) {
+    const signature = doordeck.libSodium.crypto_sign_detached(code, priKey);
+    return axios.post(doordeck.getBaseUrl() + '/auth/certificate/check', {
+        'verificationSignature': doordeck.libSodium.to_base64(signature, doordeck.libSodium.base64_variants.ORIGINAL)
+    }, {
+        headers: {
+            'Authorization': 'Bearer ' + localStorage[AUTH_TOKEN]
+        }
+    })
 }
 
 export default {
-  getCertificate (ephemeralKey) {
-    return new Promise (async function(resolve, reject) {
-      const cert = await retrieveSavedCert()
-      if (cert == null && cert == undefined) {
-        registerCertificate(ephemeralKey.publicKey)
-        .then(response => {
-          storeCert(response.data)
-          resolve({state: 'success', message: 'Doordeck SDK is loaded'})
-        })
-        .catch(fail => {
-          if (fail.response.status === 423) {
-            if (localStorage.token !== null && localStorage.token !== undefined) {
-              registerCertificate2FA(ephemeralKey.publicKey).then(response => {
-                if (response.data.method !== undefined && response.data.method !== null) {
-                  reject({state: 'verify', method: response.data.method})
-                } else reject({state: 'error', message: 'Failed to send 2FA request'})
-              })
+    getCertificate(ephemeralKey) {
+        return new Promise(async function (resolve, reject) {
+            const cert = await retrieveSavedCert()
+            if (cert == null && cert == undefined) {
+                registerCertificate(ephemeralKey.publicKey)
+                    .then(response => {
+                        storeCert(response.data)
+                        resolve({state: 'success', message: 'Doordeck SDK is loaded'})
+                    })
+                    .catch(fail => {
+                        if (fail.response.status === 423) {
+                            if (localStorage.token !== null && localStorage.token !== undefined) {
+                                registerCertificate2FA(ephemeralKey.publicKey).then(response => {
+                                    if (response.data.method !== undefined && response.data.method !== null) {
+                                        reject({state: 'verify', method: response.data.method})
+                                    } else reject({state: 'error', message: 'Failed to send 2FA request'})
+                                })
+                            }
+                        } else {
+                            reject({state: 'error', message: 'Failed to get certificate'})
+                        }
+                    })
+            } else {
+                resolve({state: 'success', message: 'Doordeck SDK is loaded'})
             }
-          } else {
-            reject({state: 'error', message: 'Failed to get certificate'})
-          }
         })
-      } else {
-        resolve({state: 'success', message: 'Doordeck SDK is loaded'})
-      }
-    })
-  },
-  verifyCode (code, ephemeralKey) {
-    return new Promise (function(resolve, reject) {
-      verify2FA(code, ephemeralKey.privateKey).then(response => {
-        storeCert(response.data)
-        resolve({state: 'success', message: 'Doordeck SDK is loaded'})
-      }, fail => {
-        reject({state: 'fail', message: 'Something went wrong'})
-      })
-    })
-  },
-  retrieveSavedCert,
-  deleteCert
+    },
+    verifyCode(code, ephemeralKey) {
+        return new Promise(function (resolve, reject) {
+            verify2FA(code, ephemeralKey.privateKey).then(response => {
+                storeCert(response.data)
+                resolve({state: 'success', message: 'Doordeck SDK is loaded'})
+            }, fail => {
+                reject({state: 'fail', message: 'Something went wrong'})
+            })
+        })
+    },
+    retrieveSavedCert,
+    deleteCert
 }
